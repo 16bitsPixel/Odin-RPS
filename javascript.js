da70c6698df86c4e4ac3025f2999a5398c6c732f
itsPixel <brandonllanes16@gmail.com>
2023-04-04 19:46:26 -0700

    Add playRound function to simulate RPS

--- javascript.js
+++ javascript.js
@@ -8,3 +8,58 @@ function getComputerChoice() {
     let random = Math.floor(Math.random() * options.length);
     return options[random];
 }
+
+/*
+    Function to play a single round of RPS
+    Parameters:
+        playerSelection - option the player picks
+        computerSelection - option the computer picks
+    Return: string to declare winner
+*/
+function playRound(playerSelection, computerSelection) {
+    let outcome;
+    switch(playerSelection.toLowerCase()) {
+        case "rock":
+            switch(computerSelection.toLowerCase()) {
+                case "rock":
+                    outcome = "Nobody wins! Both chose Rock!";
+                    break;
+                case "paper":
+                    outcome = "You lose! Paper beats Rock!";
+                    break;
+                case "scissors":
+                    outcome = "You win! Rock beats Paper!";
+                    break;
+            }
+            break;
+        case "paper":
+            switch(computerSelection.toLowerCase()) {
+                case "rock":
+                    outcome = "You win! Paper beats Rock!";
+                    break;
+                case "paper":
+                    outcome = "Nobody wins! Both chose Paper!";
+                    break;
+                case "scissors":
+                    outcome = "You lose! Scissor beats Paper!";
+                    break;
+            }
+            break;
+        case "scissors":
+            switch(computerSelection.toLowerCase()) {
+                case "rock":
+                    outcome = "You lose! Rock beats Scissors!";
+                    break;
+                case "paper":
+                    outcome = "You win! Scissors beats Paper!";
+                    break;
+                case "scissors":
+                    outcome = "Nobody wins! Both chose Scissors!";
+                    break;
+            }
+            break;
+        default:
+            outcome = "Invalid Option!";
+    }
+    return outcome;
+}
